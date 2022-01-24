2102165524955d8bfb9ea11e42b3bcbab3121a91
satvik-c <chawla.satvik07@gmail.com>
2022-01-24 17:38:03 -0600

    Update Hardware3.java
    copying gabriel's code lol
    
    pls dont arres for plagiarism ;-;
    I am good poserson I swear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware3.java
@@ -3,7 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -11,40 +12,40 @@ import java.util.*;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Hardware3 {
-    static DcMotor FLMotor;
-    static DcMotor FRMotor;
-    static DcMotor BLMotor;
-    static DcMotor BRMotor;
-    static DcMotor CarouselMotor;
+    static DcMotorEx FLMotor;
+    static DcMotorEx FRMotor;
+    static DcMotorEx BLMotor;
+    static DcMotorEx BRMotor;
+    static DcMotorEx CarouselMotor;
     static HardwareMap hardwareMap;
 
     public Hardware3(HardwareMap hardwaremap) {
         hardwareMap = hardwaremap;
     }
     public static void HardwareInit() {
-        FLMotor = hardwareMap.get(DcMotor.class, "front_left_drive");
-        FRMotor = hardwareMap.get(DcMotor.class, "front_right_drive");
-        BLMotor = hardwareMap.get(DcMotor.class, "front_right_drive");
-        BRMotor = hardwareMap.get(DcMotor.class, "front_right_drive");
-        CarouselMotor = hardwareMap.get(DcMotor.class, "carousel_drive");
+        FLMotor = hardwareMap.get(DcMotorEx.class, "front_left_drive");
+        FRMotor = hardwareMap.get(DcMotorEx.class, "front_right_drive");
+        BLMotor = hardwareMap.get(DcMotorEx.class, "front_right_drive");
+        BRMotor = hardwareMap.get(DcMotorEx.class, "front_right_drive");
+        CarouselMotor = hardwareMap.get(DcMotorEx.class, "carousel_drive");
 
-        FLMotor.setDirection(DcMotor.Direction.FORWARD);
-        FRMotor.setDirection(DcMotor.Direction.REVERSE);
-        BLMotor.setDirection(DcMotor.Direction.FORWARD);
-        BRMotor.setDirection(DcMotor.Direction.REVERSE);
-        CarouselMotor.setDirection(DcMotor.Direction.FORWARD);
+        FLMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        FRMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        BLMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        BRMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        CarouselMotor.setDirection(DcMotorEx.Direction.FORWARD);
 
-        FLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        CarouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FLMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        FRMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        BLMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        BRMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        CarouselMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
-        FLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        FRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        CarouselMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FLMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        FRMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        BLMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        BRMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        CarouselMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
 
     }
