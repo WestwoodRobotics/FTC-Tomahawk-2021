8c3593dadef1944ff0206f672d632efe8440eb09
satvik-c <chawla.satvik07@gmail.com>
2022-01-24 18:01:20 -0600

    Auton cases lol
    if Josh says this is wrong it's cool because all I did was copy and paste code :D

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCarousel.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "BlueCarousel",
+        group="linear Opmode")
+
+public class BlueCarousel extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private Hardware3 robot;
+
+    private AllenAutonFunctions allenMethods = new AllenAutonFunctions();
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        robot = new Hardware3(hardwareMap);
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive()) {
+            allenMethods.forward(24);
+            allenMethods.turnRight(180);
+            allenMethods.left(24);
+            allenMethods.forward(18);
+            allenMethods.carousel(24);
+            allenMethods.backward(18);
+        }
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+
+    }
+}
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueWarehouse.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "BlueWarehouse",
+        group="linear Opmode")
+
+public class BlueWarehouse extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private Hardware3 robot;
+
+    private AllenAutonFunctions allenMethods = new AllenAutonFunctions();
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        robot = new Hardware3(hardwareMap);
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive()) {
+            allenMethods.forward(24);
+            allenMethods.turnRight(180);
+            allenMethods.left(73);
+            allenMethods.forward(18);
+            allenMethods.carousel(24);
+            allenMethods.backward(18);
+        }
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+
+    }
+}
+
+
