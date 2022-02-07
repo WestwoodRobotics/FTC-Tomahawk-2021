0c7f00c73ca18641cd14b81e454bfd607af5100f
PlecoY <91854422+PlecoY@users.noreply.github.com>
2022-02-07 17:22:42 -0600

    epic horizontal drive teleop maybe
    maybe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -92,10 +91,10 @@ public class Teleop extends OpMode
 //        BRPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) / 2;
 
         //Power adjusting
-        FLPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) / 2;
-        FRPower = (-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) / 2;
-        BLPower = (-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x) / 2;
-        BRPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) / 2;
+        FLPower = (gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) / 2;
+        FRPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) / 2;
+        BLPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) / 2;
+        BRPower = (gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) / 2;
 
 
 
